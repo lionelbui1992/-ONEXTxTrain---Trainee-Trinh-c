0687de4fb516d0796e33cde198f5aa4476d1240d
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 14:51:02 +0700

    submit php-arrays

--- index.php
+++ index.php
@@ -2,19 +2,36 @@
 <html>
 <body>
 <?php
-$fname = "duoc";
+$arr = array("duoc", "long", "nam");
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+$cars = ["Volvo", "BMW", "Toyota"];
+$nums = [1,8,3,2];
 
-function showName($name) {
-    return $name;
+echo count($arr);
+echo "<br>";
+
+array_push($arr, "van");
+var_dump($arr);
+
+echo $car["year"] = 2000;
+
+var_dump($car);
+echo "<br>";
+foreach ($cars as $index => $c) {
+    echo $index;
 };
 
-function sum($a, $b) {
-    return $a += $b;
-};
+$cars[1] = "Honda";
 
-echo showName($fname);
 
-echo sum(1, 2);
+array_push($cars, "KIA");
+
+var_dump($cars);
+
+sort($nums);
+
+var_dump($nums);
+
 ?> 
 
 </body>
