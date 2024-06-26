4b8643e7c65c4b36c626aafc49740e70f4b92457
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 11:29:08 +0700

    submit php-datatypes

--- index.php
+++ index.php
@@ -2,9 +2,38 @@
 <html>
 <body>
 <?php
-echo "this is PHP<br>";
+$int = 10;
+$string = "PHP";
+$float = 10.1;
+$bool = true;
+$arr = array("a", "b", "c");
 
-print "this is PHP";
+var_dump($int);
+echo "<br>";
+var_dump($string);
+echo "<br>";
+var_dump($float);
+echo "<br>";
+var_dump($bool);
+echo "<br>";
+var_dump($arr);
+
+echo "<br>";
+
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("red", "Volvo");
+  var_dump($myCar);
 ?> 
 
 </body>
