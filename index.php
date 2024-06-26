353382de16f95ec5e8ee03d750b8d22d4a24676c
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 11:20:25 +0700

    submit php-echo-print

--- index.php
+++ index.php
@@ -2,39 +2,9 @@
 <html>
 <body>
 <?php
-$x = 5;
-$y = 10;
-echo $x + $y. "<br>";
-var_dump($x);
+echo "this is PHP<br>";
 
-function myTest() {
-    $x = 2;
-    echo "my number is $x";
-};
-
-myTest();
-
-echo "my number is $x";
-
-function myTest2() {
-    global $x,  $y;
-
-    $y = $x + $y;
-};
-
-myTest2();
-
-echo $y;
-
-function myTest3() {
-    static $x = 0;
-    echo $x;
-    $x++;
-}
-
-myTest3();
-myTest3();
-myTest3();
+print "this is PHP";
 ?> 
 
 </body>
