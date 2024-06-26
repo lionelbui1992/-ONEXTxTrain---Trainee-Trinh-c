96d609efcefe89bc17429fc65e250541cbc335cd
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 15:40:01 +0700

    php-superglobals

--- index.php
+++ index.php
@@ -2,37 +2,21 @@
 <html>
 <body>
 <?php
-$arr = array("duoc", "long", "nam");
-$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
-$cars = ["Volvo", "BMW", "Toyota"];
-$nums = [1,8,3,2];
+$a = 1;
 
-echo count($arr);
-echo "<br>";
-
-array_push($arr, "van");
-var_dump($arr);
-
-echo $car["year"] = 2000;
-
-var_dump($car);
-echo "<br>";
-foreach ($cars as $index => $c) {
-    echo $index;
+function myFunc() {
+    echo $GLOBALS["a"];
 };
 
-$cars[1] = "Honda";
-
-
-array_push($cars, "KIA");
-
-var_dump($cars);
-
-sort($nums);
-
-var_dump($nums);
+myFunc();
 
 ?> 
+
+<form action="process.php" method="post">
+    input name <input type="text" name="yourName" id=""> <br>
+    input email <input type="email" name="yourEmail" id=""> <br>
+    <button>Send</button>
+</form>
 
 </body>
 </html>
