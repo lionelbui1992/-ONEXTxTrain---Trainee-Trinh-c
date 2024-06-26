ff0c71960ad027f35fc838eda4a4ff8d84452a5a
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 10:45:47 +0700

    submit php-comments

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>my first php page</h1>
-
 <?php
-$color = "red";
-echo "Hello php"."<br>";
-
-echo "my car is"." ". $color;
+echo "Welcome Home!"; // Outputs a welcome message
 ?> 
 
 </body>
