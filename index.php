ae0405ff246494c9d2fab064c0d1a817f2b1552e
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 11:05:15 +0700

    submit php-variables

--- index.php
+++ index.php
@@ -2,7 +2,39 @@
 <html>
 <body>
 <?php
-echo "Welcome Home!"; // Outputs a welcome message
+$x = 5;
+$y = 10;
+echo $x + $y. "<br>";
+var_dump($x);
+
+function myTest() {
+    $x = 2;
+    echo "my number is $x";
+};
+
+myTest();
+
+echo "my number is $x";
+
+function myTest2() {
+    global $x,  $y;
+
+    $y = $x + $y;
+};
+
+myTest2();
+
+echo $y;
+
+function myTest3() {
+    static $x = 0;
+    echo $x;
+    $x++;
+}
+
+myTest3();
+myTest3();
+myTest3();
 ?> 
 
 </body>
