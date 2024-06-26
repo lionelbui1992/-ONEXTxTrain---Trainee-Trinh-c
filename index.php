6abc1a2317926918d6a8a302483c50d0dacd1c06
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 14:17:42 +0700

    submit php-loops

--- index.php
+++ index.php
@@ -2,15 +2,37 @@
 <html>
 <body>
 <?php
-$color = "red";
+$i = 1;
+$colors = array("red", "blue", "black", "orange");
+$members = array("duoc", "nam", "linh");
+while($i < 10) {
+    echo $i;
+    $i++;
+}
 
-switch($color){
-    case "red":
-        echo "your color is red";
-        break;
-    case "blue":
-        echo "your color is blue";
-        break;
+echo "<br>";
+
+for($a = 0; $a < 10; $a++) {
+    echo $a;
+}
+
+echo "<br>";
+
+foreach ($colors as $color) {
+    echo $color;
+} 
+
+echo "<br>";
+
+foreach ($members as $index => $color) {
+    echo "$index: $color" . "<br>";
+}
+
+
+$arrNums = array(1,2,3,4,5,6);
+foreach ($arrNums as $num) {
+    if($num == 5) break;
+    echo $num;
 }
 ?> 
 
