9d6df9abf53a2b2c1a9b25f5c6f675b1c4fe96e8
duoctrinh <trinhduoc69@gmail.com>
2024-06-26 13:57:59 +0700

    submit php-if-else

--- index.php
+++ index.php
@@ -4,21 +4,9 @@
 <?php
 $a = 1;
 $b = 2;
-$c = 10;
 
-$stringA = "a";
-$stringB = "b";
-
-echo $a + $b;
-echo $c *= 10;
-
-echo "<br>";
-
-var_dump($a === $b);
-
-echo "<br>";
-
-echo $stringA . $stringB;
+if($a > $b) echo "wrong";
+else echo "correct";
 ?> 
 
 </body>
