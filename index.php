b3481c7631f68a3a53cbad6d056d1893edf48554
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 10:52:34 +0700

    submit php-callback-func

--- index.php
+++ index.php
@@ -2,17 +2,27 @@
 <html>
 <body>
     <?php
-        $email = "trinhduoc@gmail.com";
-        
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) echo "email valid";
-        else echo "email not valid";
+        function sayHello($callback) {
+            $callback();
+        }
+
+        function sayGoodBye() {
+            echo "Bye";
+        }
+
+        sayHello("sayGoodBye");
 
 
-        //advanced
-        $int = 101;
-        if(filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range" => 0, "max_range" => 100)))) echo "số nguyên trong khoảng 0 đến 100";
-        else echo "không là số nguyên trong khoảng 0 đén 100";
+        function getName($fname, $lname, $callback) {
+            $name = "$fname $lname";
+            $callback($name);
+        }
+
+        function fullName($name) {
+            echo $name;
+        }
+
+        getName("trinh", "duoc", "fullname")
     ?>
 </body>
 </html>
