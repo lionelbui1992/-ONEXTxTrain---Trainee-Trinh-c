7995919ab47bb106a605200c49d6400a83e2a9f3
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 10:33:19 +0700

    submit php-filter

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-<?php
-    if (session_id() === '') session_start();
-
-    if(isset($_SESSION["counter"])) $_SESSION["counter"] += 1;
-    else $_SESSION["counter"] = 1;
-
-    echo $_SESSION["counter"];
-
-    $_SESSION["name"] = "Duoc";
-?>
 <!DOCTYPE html>
 <html>
 <body>
+    <?php
+        $email = "trinhduoc@gmail.com";
+        
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) echo "email valid";
+        else echo "email not valid";
+
+
+        //advanced
+        $int = 101;
+        if(filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range" => 0, "max_range" => 100)))) echo "số nguyên trong khoảng 0 đến 100";
+        else echo "không là số nguyên trong khoảng 0 đén 100";
+    ?>
 </body>
 </html>
