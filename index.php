9b8ecfb9f0204d06ebf1f701182de202bfe470ba
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 15:46:26 +0700

    create table

--- index.php
+++ index.php
@@ -5,15 +5,27 @@
         $hostName = "localhost";
         $userName = "root";
         $password = "";
-        
-        $conn = new mysqli($hostName, $userName, $password);
-        if($conn->connect_error) echo "connect failed";
+        $dbName = "myDB";
 
-        $sql = "CREATE DATABASE myDB";
-        if($conn->query($sql)) echo "create successfully";
-        else echo "create failed";
+        $conn = new mysqli($hostName, $userName, $password, $dbName);
 
-        $conn->close();
+        if($conn->connect_error) die("Connect failed " . $conn->connect_error);
+
+        $sql = "CREATE TABLE MyGuests (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )";
+            
+            if ($conn->query($sql) === TRUE) {
+              echo "Create table successfully";
+            } else {
+              echo "Error creating table: " . $conn->error;
+            }
+            
+            $conn->close();
     ?>
 </body>
 </html>
