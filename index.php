41bc1f9ea3f26801697ed7bea12e74cf89a81a23
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 14:01:59 +0700

    php const

--- index.php
+++ index.php
@@ -2,55 +2,15 @@
 <html>
 <body>
     <?php
-        class Animal{
-            public $name;
-            public $color;
-
-            public function __construct($name, $color) 
-            {
-                $this->name = $name;
-                $this->color = $color;
+        class Profile{
+            const MY_NAME = "Trinh Duoc";
+            public function getName() {
+                echo self::MY_NAME;
             }
-            
-            // protected function intro() {
-            //     echo "the pet is {$this->name} and color is {$this->color}";
-            // }
+        }  
 
-            public function intro() {
-                echo "the pet is {$this->name} and color is {$this->color}";
-            }
-        }
-
-        class Pig extends Animal {
-            public $weight;
-
-            public function __construct($name, $color, $weight) 
-            {
-                $this->name = $name;
-                $this->color = $color;
-                $this->weight = $weight;
-            }
-
-            public function sound() {
-                echo "Am I a pig or dog?";
-                // $this->intro();
-            } 
-
-            public function intro() {
-                echo "the pet is {$this->name} and color is {$this->color} and weight is {$this->weight}";
-            }
-        }
-
-        // $pig = new Pig("pig", "pink");
-
-        // $pig->sound();
-
-        $pig = new Pig("pig", "pink", 100);
-
-        $pig->sound();
-        $pig->intro();
+        $profile = new Profile();
+        $profile->getName();
     ?>
-
-    
 </body>
 </html>
