4a85e4892aa83c7549c6a1d3aba150b5991049a0
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 08:21:31 +0700

    submit php-include

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php
-
-    date_default_timezone_set('Asia/Ho_Chi_Minh');
-    echo "The date/time is " . date('d-m-Y H:i:s');
-
-    $date = new DateTime('27-06-2024');
-    $date->modify('+1 day');
-    echo $date->format('d-m-Y');
-
-
-    $date1 = new DateTime('28-06-2024');
-    $date2 = new DateTime('01-07-2024');
-    $diff = $date1->diff($date2);
-    echo $diff->days;
-?>
+    <?php
+        include 'header.php';
+    ?>
+    <h2>Welcome to my home page</h2>
+    <?php 
+        include 'cars.php';
+        echo "i have a $color $name";
+    ?>
 </body>
 </html>
