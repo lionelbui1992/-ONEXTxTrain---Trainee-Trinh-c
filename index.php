445fcc301d42d6c15ec82bb7d55a309acf240d1f
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 15:49:48 +0700

    insert data

--- index.php
+++ index.php
@@ -11,21 +11,16 @@
 
         if($conn->connect_error) die("Connect failed " . $conn->connect_error);
 
-        $sql = "CREATE TABLE MyGuests (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            firstname VARCHAR(30) NOT NULL,
-            lastname VARCHAR(30) NOT NULL,
-            email VARCHAR(50),
-            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-            )";
+        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+        VALUES ('Duoc', 'Trinh', 'trinhduoc@gmail.com')";
             
-            if ($conn->query($sql) === TRUE) {
-              echo "Create table successfully";
-            } else {
-              echo "Error creating table: " . $conn->error;
-            }
-            
-            $conn->close();
+        if ($conn->query($sql) === TRUE) {
+            echo "Create table successfully";
+        } else {
+            echo "Error creating table: " . $conn->error;
+        }
+        
+        $conn->close();
     ?>
 </body>
 </html>
