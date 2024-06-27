21e608554c28ee7ce8094637766c7d78db5cb1a3
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 08:10:09 +0700

    submit php-date-time

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php ?>
+<?php
+
+    date_default_timezone_set('Asia/Ho_Chi_Minh');
+    echo "The date/time is " . date('d-m-Y H:i:s');
+
+    $date = new DateTime('27-06-2024');
+    $date->modify('+1 day');
+    echo $date->format('d-m-Y');
+
+
+    $date1 = new DateTime('28-06-2024');
+    $date2 = new DateTime('01-07-2024');
+    $diff = $date1->diff($date2);
+    echo $diff->days;
+?>
 </body>
 </html>
