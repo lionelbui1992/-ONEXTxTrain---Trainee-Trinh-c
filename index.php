82bbd028b21b49030f6726d3611aa9668e551865
duoctrinh <trinhduoc69@gmail.com>
2024-06-27 15:35:14 +0700

    connect db

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 <html>
 <body>
     <?php
-        $arr = [1,2,3];
-
-        function iterable(iterable $iter) {
-            foreach($iter as $item) {
-                echo $item;
-            }
-        }
-
-        iterable($arr);
+        $hostName = "localhost";
+        $userName = "root";
+        $password = "";
+        $dbName = "dbtraining";
+        $connect = new mysqli($hostName, $userName, $password, $dbName);
+        if($connect->connect_error) echo "khong thanh cong";
+        echo "thanh cong";
     ?>
 </body>
 </html>
