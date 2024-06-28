a15733d1ba26bed71e280a8fb052049df861cb92
trinhduoc2k4 <trinhduoc2k4@gmail.com>
2024-06-28 10:09:23 +0700

    ajax live search

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <html>
 <head>
 <script>
-function showCD(str) {
-  if (str=="") {
-    document.getElementById("txtHint").innerHTML="";
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
     return;
   }
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
     if (this.readyState==4 && this.status==200) {
-      document.getElementById("txtHint").innerHTML=this.responseText;
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     }
   }
-  xmlhttp.open("GET","getcd.php?q="+str,true);
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
   xmlhttp.send();
 }
 </script>
@@ -20,15 +22,9 @@ function showCD(str) {
 <body>
 
 <form>
-Select a CD:
-<select name="cds" onchange="showCD(this.value)">
-  <option value="">Select a CD:</option>
-  <option value="Bob Dylan">Bob Dylan</option>
-  <option value="Bee Gees">Bee Gees</option>
-  <option value="Cat Stevens">Cat Stevens</option>
-</select>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
 </form>
-<div id="txtHint"><b>CD info will be listed here...</b></div>
 
 </body>
 </html>
