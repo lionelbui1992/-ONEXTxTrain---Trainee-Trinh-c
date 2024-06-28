bf808cd7d4f3258cdc9aadbd8ad82f53db3d111a
duoctrinh <trinhduoc69@gmail.com>
2024-06-28 08:44:09 +0700

    xml get

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 <?php
     $xml = simplexml_load_file("note.xml");
 
-    print_r($xml);
+    echo $xml->to . "<br>";
+    echo $xml->from . "<br>";;
+    echo $xml->heading . "<br>";;
+    echo $xml->body;
 ?>
 </body>
 </html>
