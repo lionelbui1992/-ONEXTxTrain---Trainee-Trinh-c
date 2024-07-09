d74c694a15c7317e0a21c4e2d684c4b1a84f6a3b
trinhduoc2k4 <trinhduoc2k4@gmail.com>
2024-07-09 21:53:00 +0700

    fix db

--- include/connectdb.php
+++ include/connectdb.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "porsche_experience";
+    $dbname = "pe_db";
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
